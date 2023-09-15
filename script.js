26adb6650a6bb70efce227e2273325b19a0e40a1
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-09-15 17:21:18 +0530

    demonstrate let, var & const

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hi, this is a example of console log");
+// console.log("hi, this is a example of console log");
+
+// Variables
+// ---------------
+
+var a = "this string is in var variable a";
+
+let x = "this string is in let variable x";
+const c = "this string is in const varibale c";
+
+x = "abc";
+
+console.log(x);
