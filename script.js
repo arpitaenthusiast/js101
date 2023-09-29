3334a2a54090bfb9c930059b516940ae0c475fea
Arpita Das <ping.arpita@gmail.com>
2023-09-29 13:32:44 +0530

    request for restarting class

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// console.log("hi, this is a example of console log");
-
-// Variables
-// ---------------
-
-var a = "this string is in var variable a";
-
-let x = "this string is in let variable x";
-const c = "this string is in const varibale c";
-
-x = "abc";
-
-console.log(x);
+console.log("hello world");
