b1948f1cb59b0a9664b1934690a270ca935d1ed2
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-10-08 00:09:22 +0530

    demonstrated data types

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-console.log("hello world");
+// console.log("hello world");
 
 // Comment
+
+// Variables
+// Variables are way of storing some information on the computer memory.
+
+var x = 1; // number (int in other languages)
+var y = "string"; // string
+var z = true; // boolean 
+var a = false; // boolean 
+var b = [1, 2, 5, 9, 'pq', true]; // array
+// object
+var c = {
+    first: 1,
+    second: 2,
+    third: 5,
+    fourth: 9,
+    fifth: 'pq',
+    sixth: true
+}
+var p = null; // null means empty (Please note that null is not 0 or false, it is just empty)
+var q = undefined // not clear
